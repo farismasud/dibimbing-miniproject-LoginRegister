7ce8cbb1c059333c6cefdc35ac2b8cf64c9d32d6
farismasud <masudfaris54@example.com>
2024-12-13 11:52:02 +0700

    update register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,22 +1,58 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
 const Register = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const navigate = useNavigate()
+
+    const handleChangeEmail = (a) => {
+        setEmail(a.target.value)
+    }
+    const handleChangePassword = (a) => {
+        setPassword(a.target.value)
+    }
+    const handleRegister = () => {
+        const payload = {
+            email : email,
+            password : password
+        }
+        
+    axios
+        .post("https://reqres.in/api/register", payload)
+        .then((res) => {
+            console.log(res.data)
+            localStorage.setItem('access_token', res?.data?.token)
+            setTimeout(() => {
+            navigate('/')
+            }, 2000)
+        })
+        .catch((err) => {
+            console.log(err.response)
+            console.log(err?.response?.data?.error)
+        })
+    }
+
     return (
-        <div className="wrapper">
-            <div className="container">
+        <div className='wrapper'>
+            <div className='container'>
                 <h1>Register</h1>
-                <div className="form-container">
-                    <p>Username</p>
-                    <input type="text" placeholder="Username" />
+                <div className='form-container'>
+                    
+                    <p>Email</p>
+                    <input onChange={handleChangeEmail} type="text" placeholder='email'/>
+                    
                     <p>Password</p>
-                    <input type="password" placeholder="Password" />
-                    <button>Register</button>
+                    <input onChange={handleChangePassword} type="text" placeholder='password'/>
+
+                    <button onClick={handleRegister}>Register</button>
                 </div>
-                <p>Already have an account? <Link to="/login">Login</Link></p>
+                <Link to='/login'>already have an account</Link>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Register;
+export default Register
